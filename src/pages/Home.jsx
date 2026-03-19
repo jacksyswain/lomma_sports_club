30efb48573cec9bdf3a078ed1fb1f697b1da07c8
jyotiprakash swain <jp.swainabc@gmail.com>
2026-03-19 23:59:34 +0530

    initial setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,7 @@ export default function Home() {
   return (
     <div className="home">
       <div className="hero">
-        <video autoPlay loop muted className="hero-video">
-          <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4" />
-        </video>
-
+        
         <div className="overlay">
           <motion.h1 initial={{ y: 50, opacity: 0 }} animate={{ y: 0, opacity: 1 }}>
             Welcome to Loma Sports Club
