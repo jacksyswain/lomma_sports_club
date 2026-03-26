c37fefa4ccdea5a3beac3549441c3ffff88e2137
jyotiprakash swain <jp.swainabc@gmail.com>
2026-03-27 01:21:29 +0530

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
 import Teams from "./pages/Teams";
-import Matches from "./pages/Matches";
+
 import About from "./pages/About";
 import Gallery from "./pages/Gallery";
 
@@ -20,7 +20,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/teams" element={<Teams />} />
-          <Route path="/matches" element={<Matches />} />
+         
           <Route path="/gallery" element={<Gallery />} />
           <Route path="/about" element={<About />} />
         </Routes>
